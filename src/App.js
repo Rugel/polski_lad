72e5e1a01e8d8eb3a144f15ae86959b71700fbfe
gdychala <g.dychala@gmail.com>
2024-01-16 19:41:14 +0100

    added og links

--- src/App.js
+++ src/App.js
@@ -171,7 +171,9 @@ class App extends React.Component {
 
     return <div>
       <header><Wynik />
-        <nav><div id="tytul"><u><h1>Kalkulator Wynagrodzeń</h1></u><br /><div className='box'><strong><p>- dla pracowników zatrudnionych na umowę o pracę rozliczanych za pomocą stawki godzinowej</p><p>- przelicznik BRUTTO na NETTO</p></strong></div>
+        <nav><div id="tytul">
+        <div className="fb-share-button" data-href="https://stawka-godzinowa.pl/" data-layout="" data-size=""><a target="blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fstawka-godzinowa.pl%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Udostępnij</a></div>
+          <u><h1>Kalkulator Wynagrodzeń</h1></u><br /><div className='box'><strong><p>- dla pracowników zatrudnionych na umowę o pracę rozliczanych za pomocą stawki godzinowej</p><p>- przelicznik BRUTTO na NETTO</p></strong></div>
         </div>
         </nav>
       </header>
